9b1c7e56a761f0ee10e312c00c16e74bf8a7a166
Tadej C <tadej.ciglic@gmail.com>
2019-01-19 20:02:55 +0100

    project start

---
+++ ucenje.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+
+def sestevanje():
+    p = 0  # seštevanje pravilnih odgovorov
+    n = 0  # seštevanje nepravilnih odgovorov
+    print("Reši naloge seštevanja.")
+    for y in range(1, 11):
+        import random
+        num1 = random.randint(1,101)
+        num2 = random.randint(1,100 - num1) #zagotovimo z drugo spremenljivko, da ne gremo z seštevanjem preko števila 100
+
+        print("Rezultat        Pravilni: " + str(p) + "       Nepravilni: " + str(n))
+        print(str(num1) + " + " + str(num2) + " = ")
+
+        while True:
+            try:
+                rez = int(input("Ogovor: \n"))
+                if rez == num1 + num2:
+                    print("ODLIČNO!! Odgovor je pravilen!\n")
+                    p = p + 1 #sešteva pravilne odgovore
+                    break
+                else:
+                    print("Žal, nepravilno!\n")
+                    n = n + 1
+                    break
+            except ValueError:
+                print("Vnesi celo številko.")
+
+    print("Rezultat        Pravilni: " + str(p) + "       Nepravilni: " + str(n))
+    vprasanje = input("Ali želiš ponoviti nalogo seštevanja? (da/ne)")
+    if vprasanje == "da":
+        sestevanje()
+    else:
+        print("Hvala za reševanje.\n")
+
+def odstevanje():
+    p = 0  # seštevanje pravilnih odgovorov
+    n = 0  # seštevanje nepravilnih odgovorov
+    print("Reši naloge odštevanja.")
+
+    for y in range(1, 11):
+        import random
+        num1 = random.randint(1,101)
+        num2 = random.randint(1,num1) #z drugo vrednostjo določimo max številko, ki ne sme presegati prve
+
+        print("Rezultat        Pravilni: " + str(p) + "       Nepravilni: " + str(n))
+        print(str(num1) + " - " + str(num2) + " = ")
+
+        while True:
+            try:
+                rez = int(input("Ogovor: \n"))
+                if rez == num1 - num2:
+                    print("ODLIČNO!! Odgovor je pravilen!\n")
+                    p = p + 1 #sešteva pravilne odgovore
+                    break
+                else:
+                    print("Žal, nepravilno!\n")
+                    n = n + 1
+                    break
+            except ValueError:
+                print("Vnesi celo številko.")
+
+    print("Rezultat        Pravilni: " + str(p) + "       Nepravilni: " + str(n))
+    vprasanje = input("Ali želiš ponoviti nalogo odštevanja? (da/ne)")
+    if vprasanje == "da":
+        odstevanje()
+    else:
+        print("Hvala za reševanje.\n")
+
+print("Pozdravljen/a v programu za matematiko.\n")
+while True:
+    try:
+        izbira = int(input(" Za seštevanje pritisni 1\n Za odštevanje pritisni 2\n Za izhod pritisni 3\n"))
+        if izbira == 1:
+            sestevanje()
+        elif izbira == 2:
+            odstevanje()
+        elif izbira == 3:
+            print("Želim ti lep dan.")
+            input("Pritisni ENTER za izhod")
+            quit()
+    except ValueError:
+        print("Izberi število 1, 2 ali 3\n")
+
+
+
